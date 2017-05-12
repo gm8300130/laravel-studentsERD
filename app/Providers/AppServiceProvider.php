0626d0ba478c0a7de29909168236636ecb1507da
小寶 <gm8300130@gmail.com>
2017-05-12 18:56:33 +0800

    debugbar

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
 	
+	
 	/**
 	* Bootstrap any application services.
 	*
@@ -14,17 +15,20 @@ class AppServiceProvider extends ServiceProvider
 	*/
 	public function boot()
 	{
-		//
+	//
 	}
+	
 	
 	
 	/**
 	* Register any application services.
-    *
-    * @return void
-    */
+	*
+	* @return void
+	*/
 	public function register()
 	{
-
+		if ($this->app->environment() == 'local') {
+			$this->app->register('Barryvdh\Debugbar\ServiceProvider');
+		}
 	}
 }
