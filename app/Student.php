33186692eec7fb8e1eee8e57e37f35f1f4b60efa
小寶 <gm8300130@gmail.com>
2017-05-15 12:27:35 +0800

    edit model

--- app/Student.php
+++ app/Student.php
@@ -7,5 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     protected $table = 'student';
+    
     public $timestamps = false;
+    
+    public function scopeOfStudentId($query, $student_id = null)
+    {
+        return $student_id ? $query->where('id', $student_id) : null;
+    }
+    
+    public function scopeOfRegisterDate($query, $register_date = null)
+    {
+        return $register_date ? $query->where('registerDate', $register_date) : null;
+    }
+    
+    public function scopeOfName($query, $name = null)
+    {
+        return $name ? $query->where('name', $name) : null;
+    }
+    
+    public function scopeOfStudentIdStartLimit($query, $startRowNumber, $limit = 0)
+    {
+        return  $query->where('id', '>=', $startRowNumber)
+            ->take($limit);
+    }
+    
 }
