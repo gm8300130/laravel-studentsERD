401514f31506a98ea4550cc6cdd530a472ca9ead
小寶 <gm8300130@gmail.com>
2017-05-15 12:24:44 +0800

    add api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::get('/v1/students/students_course_grade', 'StudentErdController@queryStudentsCourseGrade');
+
+Route::get('/v1/students/{studentId}', 'StudentErdController@queryStudent');
+
+Route::post('/v1/students/method/query', 'StudentErdController@queryStudentConditions');
+
+Route::any('/v1/students/method/start/{startRowNumber}/limit/{limit}', 'StudentErdController@queryStudentNumberLimit')
+    ->where('startRowNumber', '[0-9]+')
+    ->where('limit', '[0-9]+');
